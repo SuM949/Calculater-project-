705e1b1c94c61cb2a519199d688ad127b7fd5933
Sumaya Gure <sumayabile8@gmail.com>
2023-04-11 18:52:13 +0200

    adding some js

--- app.js
+++ app.js
@@ -5,15 +5,24 @@
     let equal = document.querySelector('.btn-equal');
       
 
-    // to check whos button was pressed 
+    // to check the button is pushed in the screen 
     buttons.forEach(function(button){
         button.addEventListener('click',function(e){
            let value = e.target.dataset.num;
            screen.value += value;
         })
-    })
+    });
 
-
+  equal.addEventListener('click', function(e){
+    if(screen.value === ''){
+        screen.value = "please enter";
+    }else{
+        let answer = eval(screen.value);
+        screen.value = answer;
+    }
+  })
+  clear.addEventListener('click',function(e){
+    screen.value = ands
 
 
 
