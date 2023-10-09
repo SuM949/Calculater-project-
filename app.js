48936ac4a6cc7a8f69d443b6976c20d546cd6e51
Sumaya Gure <sumayabile8@gmail.com>
2023-10-09 21:06:35 +0200

    adding js

--- app.js
+++ app.js
@@ -1,30 +1,38 @@
-(function(){
-    let screen = document.querySelector('.screen');
-    let buttons = document.querySelectorAll('.btn');
-    let clear = document.querySelector('.btn-clear');
-    let equal = document.querySelector('.btn-equal');
-      
+(function () {
+  let screen = document.querySelector('.screen');
+  let buttons = document.querySelectorAll('.btn');
+  let clear = document.querySelector('.btn-clear');
+  let equal = document.querySelector('.btn-equal');
 
-    // to check the button is pushed in the screen 
-    buttons.forEach(function(button){
-        button.addEventListener('click',function(e){
-           let value = e.target.dataset.num;
-           screen.value += value;
-        })
+  // Check the button is pushed in the screen 
+  buttons.forEach(function (button) {
+      button.addEventListener('click', function (e) {
+          let value = e.target.innerText;
+          screen.value += value;
+      })
+  });
+  equal.addEventListener('click', function (e) {
+    if (screen.value === '') {
+        screen.value = "";
+    } else {
+
+      let expression = screen.value.replace(/=$/, '');
+        console.log("Before eval:", expression);
+
+        try {
+            let answer = eval(expression);
+            console.log("After eval:", answer);
+
+            if (isNaN(answer) || !isFinite(answer)) {
+                console.error("Invalid result:", answer);
+                screen.value = "Error";
+            } else {
+                screen.value = '' + answer;
+            }
+        } catch (error) {
+            console.error("Evaluation error:", error);
+            screen.value = "Error";
+        }
+      }
     });
-
-  equal.addEventListener('click', function(e){
-    if(screen.value === ''){
-        screen.value = "please enter";
-    }else{
-        let answer = eval(screen.value);
-        screen.value = answer;
-    }
-  })
-  clear.addEventListener('click',function(e){
-    screen.value = ands
-
-
-
 })();
-
